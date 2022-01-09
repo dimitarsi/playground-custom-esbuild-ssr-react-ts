253fd88d791a08fdbf7e94557661a7e870548aed
Dimitar iliev <dimitar.iliev@aboutyou.com>
2022-01-09 14:07:36 +0100

    chore: remove build files

--- build/DeferredComponent-BKOVVPWO.js
+++
@@ -1,2 +0,0 @@
-import{c as n,e as r}from"./chunk-QP4VVJO2.js";var t=n(r());function l(){let[o,e]=(0,t.useState)("Hello from a promise");return t.default.createElement(t.default.Fragment,null,t.default.createElement("div",null,o),t.default.createElement("button",{onClick:()=>e("Hello World")},"Click to set the text"))}export{l as default};
-//# sourceMappingURL=DeferredComponent-BKOVVPWO.js.map
--- build/DeferredComponent-F62LMYLD.js
+++
@@ -1,2 +0,0 @@
-import{e as a}from"./chunk-O6ZKBB5D.js";export{a as default};
-//# sourceMappingURL=DeferredComponent-F62LMYLD.js.map
